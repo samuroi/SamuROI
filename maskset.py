cff3be7b3acb83a763e806478c00db34035a029f
enigma <enigma@nbubu.de>
2016-09-05 11:31:58 +0200

    [cleanup] remove obsolete MaskSelection class

--- maskset.py
+++ maskset.py
@@ -36,7 +36,7 @@ class MaskSet(MutableSet):
                 yield i
                 # loop over children if the mask has children:)
                 # fixme actually this requires recursion...
-                for child in getattr(i,"children",[]):
+                for child in getattr(i, "children", []):
                     yield child
 
     def __getitem__(self, type):
@@ -59,28 +59,3 @@ class MaskSet(MutableSet):
 
     def types(self):
         return self.__items.keys()
-
-
-class MaskSelection(MaskSet):
-    """Special handling for selection, adapts the set according to hierarchic relation between the masks"""
-
-    def __init__(self, iterable=[]):
-        super(MaskSelection, self).__init__(iterable)
-
-    def discard(self, elem):
-        """If there are elements contained which have the discareded element as parent, remove those elements as well."""
-        to_remove = []
-        for potential_child in self:
-            if hasattr(potential_child, "parent") and potential_child.parent is elem:
-                to_remove.append(potential_child)
-        # remove the children first
-        for child in to_remove:
-            self.discard(child)
-        # call base function to remove parent
-        MaskSet.discard(self, elem)
-
-    def add(self, elem):
-        """Add parent to selection if element has parent"""
-        if hasattr(elem, "parent"):
-            self.add(elem.parent)
-        MaskSet.add(self, elem)
