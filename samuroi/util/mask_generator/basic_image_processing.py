7f3308786b8cea62feb1bde4b125e5dea033db8f
Stephen Lenzi <stephen.lenzi@gmail.com>
2017-03-26 22:18:14 +0100

    simplified smoothing function, added documentation

--- samuroi/util/mask_generator/basic_image_processing.py
+++ samuroi/util/mask_generator/basic_image_processing.py
@@ -6,28 +6,28 @@ import cv2
 
 def smooth_data(data, iterations=4):
     """smooth image stack
-    ::param data: NxMxF ndarray, where F is the number of frames and NxM are the image dimensions
-    ::param iterations: number of times to run the smoothing
+
+    :param data: NxMxF ndarray, where F is the number of frames and NxM are the image dimensions
+    :param iterations: number of times to run the smoothing
+    :return sv: smoothed data
     """
-    # function for smoothing a single video
-    sv = np.zeros_like(data)
-    for i, d in enumerate(data.T):
+    sv = data
+    for i in range(iterations):
         sv[:, :, i] = cv2.blur(d.T, (3, 3))
-    iterations -= 1
-    if iterations == 0:
-        return sv
-    else:
-        sv = smooth_data(sv, iterations=iterations)
     return sv
 
 
 def std_image(data):
-    """::param data: NxMxF ndarray, where F is the number of frames and NxM are the image dimensions"""
+    """
+    :param data: NxMxF ndarray, where F is the number of frames and NxM are the image dimensions
+    :return std_image: a z-stack standard deviation image
+
+    """
     smoothed = smooth_data(data)  # generate smoothened NxMxF array
     std_image = np.std(smoothed, axis=2)
     return std_image
 
 
 def sum_image(data):
-    """::param data: NxMxF ndarray, where F is the number of frames and NxM are the image dimensions"""
+    """:param data: NxMxF ndarray, where F is the number of frames and NxM are the image dimensions"""
     return np.sum(data, axis=2)
