32876e196f72e65fd51159a5ce6ec0d958d8a67d
Martin Rückl <mrueckl@deloitte.de>
2019-03-27 12:55:18 +0100

    update and consolidate dependencies in setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ setup(
     description='Segmentation and Analysis of Multiple User-defined ROIs',
     author='Martin Rueckl',
     author_email='enigma@nbubu.de',
-    url='https://github.com/aolsux/SamuROI',
-    download_url='https://github.com/aolsux/SamuROI/tarball/0.1',
+    url='https://github.com/samuroi/SamuROI',
     keywords=['ROI', 'data exploration', 'image', 'segmentation', 'event detection'],
     classifiers=[],
     install_requires=[
-        'numpy', 'h5py', 'scikit-image', 'matplotlib', 'pillow'
+        'numpy>=1.16.2',
+        'h5py>=2.9.0',
+        'matplotlib>=3.0.0',
+        'pillow>=5.4.1',
+        'pyqt5>=5.12.1',
+        'cached_property'
     ],
 )
