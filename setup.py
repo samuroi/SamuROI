ee909dcab2313099d79af3b089a031aa8c630705
Martin Rueckl <enigma@nbubu.de>
2019-04-09 15:20:04 +0200

    fix deployment credentials

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='samuroi',
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='Segmentation and Analysis of Multiple User-defined ROIs',
     author='Martin Rueckl',
